c234f809110aff680391e8c87000dcb4df7e9d75
rishabhdhayal <76101214+rishabhdhayal@users.noreply.github.com>
2023-01-02 20:08:01 +0530

    script css upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// const anime = require("animejs");
+
+const aboutus = document.getElementById('aboutus');
+const page1 = document.getElementById('first_page');
+const page2 = document.getElementById('second_page');
+const page3 = document.getElementById('third_page');
+const page4 = document.getElementById('fourth_page');
+const page5 = document.getElementById('fifth_page');
+// const elements = document.getElementsByClassName('first_page second_page third_page fourth_page fifth_page footer bc_image')
+const video = document.getElementById('video');
+const cancel = document.getElementById('cancel');
+const x = document.getElementsByTagName('iframe');
+
+const goaway = ()=>{
+    event.preventDefault();
+first_page.style.display = 'none';
+video.style.display = 'block';
+x.pl
+}
+const govideo = () =>
+{
+    event.preventDefault();
+    first_page.style.display = 'block';
+video.style.display = 'none';
+}
+
+aboutus.addEventListener('click',goaway);
+cancel.addEventListener('click',govideo);
+
+const burger = document.getElementById('burger');
+const backHome = document.getElementById('pnc');
+const body1 = document.getElementById('body1');
+const p_nav = document.getElementById('p_nav');
+const p_elements = document.getElementById('p_a');
+const p_elements1 = document.getElementById('p_a1');
+const p_elements2 = document.getElementById('p_a2');
+const p_elements3 = document.getElementById('p_a3');
+
+const goaway1 = ()=>{
+    event.preventDefault();
+p_nav.style.display = 'block';
+}
+const govideo1 = () =>
+{
+    event.preventDefault();
+p_nav.style.display = 'none';
+}
+
+const scroll = (e)=>
+{
+    // event.preventDefault();
+    p_nav.style.display ='none';
+    e.scrollIntoView(true);
+}
+burger.addEventListener('click',goaway1);
+pnc.addEventListener('click',govideo1);
+// p_elements.addEventListener('click',scroll(page1));
+
+function myfun()
+{
+    event.preventDefault();
+    page3.scrollIntoView();
+    p_nav.style.display = 'none';
+}
+function myfun1()
+{
+    event.preventDefault();
+    page2.scrollIntoView();
+    p_nav.style.display = 'none';
+}
+function myfun2()
+{
+    event.preventDefault();
+    // page3.scrollIntoView();
+    p_nav.style.display = 'none';
+}
+function myfun3()
+{
+    event.preventDefault();
+    page5.scrollIntoView();
+    p_nav.style.display = 'none';
+}
+
+function dp()
+{
+    alert('In development process. ');
+}
